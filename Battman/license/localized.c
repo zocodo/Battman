f9196c2379a8129d942016dac10ec2e904cdc3dc
Ruphane <24724395+LNSSPsd@users.noreply.github.com>
2025-04-30 08:05:06 -0700

    what

--- Battman/license/localized.c
+++
@@ -1,9 +0,0 @@
-#if !defined(USE_GETTEXT)
-// This file is not designed for compile, but providing strings for xgettext.
-#include "common.h"
-
-const char agreeStr[]   = _ID_("Agree");
-const char declineStr[] = _ID_("Decline");
-const char licenseStr[] = _ID_("License");
-const char licenseStr[] = _ID_("Terms of Use");
-#endif
