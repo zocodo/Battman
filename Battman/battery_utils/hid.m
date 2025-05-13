45b878931e121f016f6178338bd9f771d25cfcfc
Torrekie <31795847+Torrekie@users.noreply.github.com>
2025-05-13 15:30:02 +0800

    fix typo

--- Battman/battery_utils/hid.m
+++ Battman/battery_utils/hid.m
@@ -16,7 +16,7 @@
 #endif
 
 #if __has_include(<IOKit/hid/IOHIDEventTypes.h>)
-#incldue <IOKit/hid/IOHIDEventTypes.h>
+#include <IOKit/hid/IOHIDEventTypes.h>
 #else
 #define kIOHIDEventTypeTemperature 15
 #define kIOHIDEventFieldTemperatureLevel (kIOHIDEventTypeTemperature << 16)
@@ -49,7 +49,7 @@ CF_IMPLICIT_BRIDGING_DISABLED
 #endif
 
 // Sadly this is SPI
-#if __has_include(<IOKit/hid/IOHIDEventSystemClient.h>)
+#if __has_include(<IOKit/hid/IOHIDEventSystemClient.h>) && 0
 #include <IOKit/hid/IOHIDEventSystemClient.h>
 #else
 CF_IMPLICIT_BRIDGING_ENABLED
